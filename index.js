34c628378d7e16d8358a492720735e247101079e
mariaasghar123 <mariaasghar74@gmail.com>
2025-09-24 14:01:57 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// document.getElementById('loginForm').addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   const username = e.target.username.value.trim();
+//   const password = e.target.password.value.trim();
+
+//   // Yahan aap backend API call kar sakti ho
+//   console.log('Username:', username, 'Password:', password);
+
+//   // Example:
+//   // fetch('/api/auth/login', {
+//   //   method: 'POST',
+//   //   headers: { 'Content-Type': 'application/json' },
+//   //   body: JSON.stringify({ username, password })
+//   // })
+//   // .then(res => res.json())
+//   // .then(data => console.log(data));
+// });
